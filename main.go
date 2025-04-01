08d9811af6eda954fc4abdd3ee1bcfa84f8f03e4
dajuf7 <daniel7lizarazo@gmail.com>
2025-04-01 15:53:27 -0500

    feat(bot que juega y empate)

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"embed"
 	"flag"
+	"fmt"
 	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
@@ -15,28 +17,137 @@ var port = flag.String("addr", ":1718", "http service address")
 
 var t = template.Must(template.ParseFS(htmlTemplates, "web/templates/*"))
 
-var tablero = NuevoTablero()
+type EstadoJuego int
+
+const (
+	EnJuego = iota
+	Ganado
+	Perdido
+	Empate
+)
+
+type EstadoTriqui struct {
+	TableroActual Tablero
+	Estado        EstadoJuego
+	TrioGanador   Trio
+}
+
+var estadoTriqui = EstadoTriqui{
+	TableroActual: NuevoTablero(),
+	Estado:        EnJuego,
+	TrioGanador:   Trio{},
+}
+
+func (tablero Tablero) ObtenerDisponibles() []int {
+	disponibles := make([]int, 0, 9)
+	k := 0
+	for i := range tablero {
+		if tablero[i] != Vacio {
+			continue
+		}
+		disponibles = append(disponibles, i)
+		k++
+	}
+	return disponibles
+}
+
+func (tablero *Tablero) JugarCelda() {
+	disponibles := tablero.ObtenerDisponibles()
+	rangoDisponible := len(disponibles)
+	indiceCeldaDisponible := rand.Intn(rangoDisponible)
+	indiceCeldaAJugar := disponibles[indiceCeldaDisponible]
+	tablero[indiceCeldaAJugar] = O
+}
+
+// Los valores de rune del 48 al 57 corresponden a los numeros 0 al 9
+// El string se convierte en un array de runes, y se toma el primer valore
+// que es el unico que nos interesa, luego a ese valor se le resta 48
+// que es el valor de "0"
+func DigitoRune(s string) (int, error) {
+	if len(s) <= 0 {
+		return 0, fmt.Errorf("El string está vacio")
+	}
+	numero := []rune(s)[0] - 48
+	if numero < 0 || numero > 9 {
+		return 0, fmt.Errorf("El string %s tiene el valor %c por lo que no es un número", s)
+	}
+
+	return int(numero), nil
+}
 
 func main() {
-
-	tablero[1] = X
-	tablero[5] = O
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./web/static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		t.ExecuteTemplate(w, "index.html", tablero)
+
+		if err := t.ExecuteTemplate(w, "index.html", estadoTriqui); err != nil {
+			log.Print(err.Error())
+		}
+
 	})
 
 	http.HandleFunc("/{indice}", func(w http.ResponseWriter, r *http.Request) {
-		// Los valores de rune del 48 al 57 corresponden a los numeros 0 al 9
-		indice := int([]rune(r.PathValue("indice"))[0]) - 48
-		if indice < 0 || indice > 8 {
-			t.ExecuteTemplate(w, "index.html", tablero)
+		indice, err := DigitoRune(r.PathValue("indice"))
+
+		if err != nil {
 			return
 		}
-		tablero[indice] = X
-		t.ExecuteTemplate(w, "index.html", tablero)
+
+		estadoTriqui.TableroActual[indice] = X
+
+		resultadoTrio, ganador := estadoTriqui.TableroActual.EstablecerGanador()
+
+		if ganador == X {
+			estadoTriqui.Estado = Ganado
+			estadoTriqui.TrioGanador = resultadoTrio
+
+			if err := t.ExecuteTemplate(w, "index.html", estadoTriqui); err != nil {
+				log.Print(err.Error())
+			}
+
+			return
+		}
+
+		disponibles := estadoTriqui.TableroActual.ObtenerDisponibles()
+
+		if len(disponibles) == 0 {
+			estadoTriqui.Estado = Empate
+
+			if err := t.ExecuteTemplate(w, "index.html", estadoTriqui); err != nil {
+				log.Print(err.Error())
+			}
+
+			return
+		}
+
+		estadoTriqui.TableroActual.JugarCelda()
+		resultadoTrio, ganador = estadoTriqui.TableroActual.EstablecerGanador()
+
+		if ganador == O {
+			estadoTriqui.Estado = Perdido
+			estadoTriqui.TrioGanador = resultadoTrio
+
+			if err := t.ExecuteTemplate(w, "index.html", estadoTriqui); err != nil {
+				log.Print(err.Error())
+			}
+
+			return
+		}
+
+		if err := t.ExecuteTemplate(w, "index.html", estadoTriqui); err != nil {
+			log.Print(err.Error())
+		}
+
+	})
+
+	http.HandleFunc("/resetear", func(w http.ResponseWriter, r *http.Request) {
+
+		estadoTriqui.TableroActual = NuevoTablero()
+		estadoTriqui.Estado = EnJuego
+
+		http.Redirect(w, r, "/", http.StatusFound)
+
 	})
 
 	log.Println("listening on", *port)
--- triqui.go
+++ triqui.go
@@ -8,8 +8,10 @@ const (
 
 type Tablero [9]rune
 
-func (tablero *Tablero) compararTres(i, j, k int) bool {
-	return tablero[i] == tablero[j] && tablero[j] == tablero[k]
+type Trio [3]int
+
+func (tablero *Tablero) compararTres(trio Trio) bool {
+	return tablero[trio[0]] == tablero[trio[1]] && tablero[trio[1]] == tablero[trio[2]]
 }
 
 func NuevoTablero() Tablero {
@@ -20,7 +22,19 @@ func NuevoTablero() Tablero {
 	return tablero
 }
 
-func (tablero *Tablero) EstablecerGanador() rune {
+func NuevoCeldasDisponibles() []int {
+	celdasDisponibles := make([]int, 9)
+	for i := 0; i < len(celdasDisponibles); i++ {
+		celdasDisponibles[i] = i
+	}
+	return celdasDisponibles
+}
+
+func (trio *Trio) AsignarTrio(i, j, k int) {
+	trio[0], trio[1], trio[2] = i, j, k
+}
+
+func (tablero *Tablero) EstablecerGanador() (Trio, rune) {
 	// Verificaremos solo las primera fila y columna
 	// En 0 y en 2 verificaremos las diagonales
 	//
@@ -30,27 +44,31 @@ func (tablero *Tablero) EstablecerGanador() rune {
 	//
 	// Si 0 es vacio no se verifican ni filas ni columnas pero si tiene algo verificamos
 	// En 0 se puede ganar con una fila, una columna y una diagonal
+	var trio Trio
 	if tablero[0] != Vacio {
 		// Verificamos la fila
 		// 0|1|2      X|X|X
 		// 3|4|5  ->  3|4|5
 		// 6|7|8      6|7|8
-		if tablero.compararTres(0, 1, 2) {
-			return tablero[0]
+		trio.AsignarTrio(0, 1, 2)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 		// Verificamos la columna
 		// 0|1|2      X|1|2
 		// 3|4|5  ->  X|4|5
 		// 6|7|8      X|7|8
-		if tablero.compararTres(0, 3, 6) {
-			return tablero[0]
+		trio.AsignarTrio(0, 3, 6)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 		// Verificamos la diagonal
 		// 0|1|2      X|1|2
 		// 3|4|5  ->  3|X|5
 		// 6|7|8      6|7|X
-		if tablero.compararTres(0, 4, 8) {
-			return tablero[0]
+		trio.AsignarTrio(0, 4, 8)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 	}
 	// Seguimos por la misma fila comparando 1,
@@ -60,8 +78,9 @@ func (tablero *Tablero) EstablecerGanador() rune {
 		// 0|1|2      0|X|2
 		// 3|4|5  ->  3|X|5
 		// 6|7|8      6|X|8
-		if tablero.compararTres(1, 4, 7) {
-			return tablero[1]
+		trio.AsignarTrio(1, 4, 7)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 	}
 	// Seguimos por la misma fila comparando 2,
@@ -71,15 +90,17 @@ func (tablero *Tablero) EstablecerGanador() rune {
 		// 0|1|2      0|1|X
 		// 3|4|5  ->  3|4|X
 		// 6|7|8      6|7|X
-		if tablero.compararTres(2, 5, 8) {
-			return tablero[2]
+		trio.AsignarTrio(2, 5, 8)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 		// Verificamos la diagonal
 		// 0|1|2      0|1|X
 		// 3|4|5  ->  3|X|5
 		// 6|7|8      X|7|8
-		if tablero.compararTres(2, 4, 6) {
-			return tablero[2]
+		trio.AsignarTrio(2, 4, 6)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 	}
 	// Seguimos por la primera columna comparando 3,
@@ -89,8 +110,9 @@ func (tablero *Tablero) EstablecerGanador() rune {
 		// 0|1|2      0|1|2
 		// 3|4|5  ->  X|X|X
 		// 6|7|8      6|7|8
-		if tablero.compararTres(3, 4, 5) {
-			return tablero[3]
+		trio.AsignarTrio(3, 4, 5)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 	}
 	// Seguimos por la misma columna comparando 6,
@@ -101,9 +123,10 @@ func (tablero *Tablero) EstablecerGanador() rune {
 		// 0|1|2      0|1|2
 		// 3|4|5  ->  3|4|5
 		// 6|7|8      X|X|X
-		if tablero.compararTres(6, 7, 8) {
-			return tablero[6]
+		trio.AsignarTrio(6, 7, 8)
+		if tablero.compararTres(trio) {
+			return trio, tablero[trio[0]]
 		}
 	}
-	return Vacio
+	return trio, Vacio
 }
